7ca824a883d29cdeb5de4da18ce0e6f361e1e69c
yanxi zhu <547110356@qq.com>
2019-09-25 20:07:57 +0800

    Update Home/Index

--- ConsumerWeb/Controllers/HomeController.cs
+++ ConsumerWeb/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ConsumerWeb.Models;
+using Consul;
+using System.Net.Http;
 
 namespace ConsumerWeb.Controllers
 {
@@ -18,8 +20,28 @@ namespace ConsumerWeb.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var consulUrl = "http://localhost:8500";
+            using (var consulClient = new ConsulClient(t => t.Address = new Uri(consulUrl)))
+            {
+                var services = consulClient.Catalog.Service("ServiceA").Result.Response;
+                if (services != null && services.Any())
+                {
+                    // 模拟随机一台进行请求
+                    Random r = new Random();
+                    int index = r.Next(services.Count());
+                    var service = services.ElementAt(index);
+
+                    using (HttpClient client = new HttpClient())
+                    {
+                        var response = await client.GetAsync($"http://{service.ServiceAddress}:{service.ServicePort}/WeatherForecast");
+                        var result = await response.Content.ReadAsStringAsync();
+                        ViewBag.Weather = $"Port at {service.ServicePort}:{result}";
+                    }
+                }
+            }
+
             return View();
         }
 
